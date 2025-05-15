2ee966aa0147eee904dc1bfc464d241b66c8a0f8
drd5166 <u22ec080@eced.svnit.ac.in>
2025-05-15 10:42:07 +0530

    Face recognition files

---
+++ datatable.sql
@@ -0,0 +1,13 @@
+-- Step 1: Create the database
+CREATE DATABASE Authorized_user;
+
+-- Step 2: Use the database
+USE Authorized_user;
+
+-- Step 3: Create the table
+CREATE TABLE my_table (
+    id INT PRIMARY KEY,
+    name VARCHAR(255),
+    age INT,
+    address VARCHAR(255)
+);
